d6895550ed4cea32e1b91dbc463b245978bbbd31
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-07-15 16:38:54 +0900

    feat: add crud apis

--- backend/models/Applications.js
+++ backend/models/Applications.js
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongodb";
+
 import { APPLICATION_STATUS } from "../constants/applications.js";
 import { getDB } from "../db.js";
 
@@ -22,4 +24,23 @@ export async function createUser({
     applicationDate: new Date(),
   });
   return { _id: insertedId };
+}
+
+export function getAllApplications() {
+  return getApplicationsCollection().find().toArray();
+}
+
+export function updateApplicationById(applicationId, updates) {
+  return getApplicationsCollection().findOneAndUpdate(
+    { _id: new ObjectId(applicationId) },
+    { $set: updates },
+    { returnDocument: "after" },
+  );
+}
+
+export async function deleteApplicationById(applicationId) {
+  const { deletedCount } = await getApplicationsCollection().deleteOne({
+    _id: new ObjectId(applicationId),
+  });
+  return deletedCount;
 }
