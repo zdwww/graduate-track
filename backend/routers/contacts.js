b7bfb31cc1bc4744744aac676a9e52311185d65c
zdwww <zdw199948@icloud.com>
2026-07-16 00:19:37 -0400

    feat: add contacts crud api

--- backend/routers/contacts.js
+++ backend/routers/contacts.js
@@ -1,9 +1,20 @@
 import express from "express";
 
-import { getContacts } from "../controllers/contacts.js";
+import {
+  createNewContact,
+  getContacts,
+  getContact,
+  updateContact,
+  deleteContact,
+} from "../controllers/contacts.js";
+import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/contacts", getContacts);
+router.get("/contacts", requireAuth, getContacts);
+router.post("/contacts", requireAuth, createNewContact);
+router.get("/contact/:contactId", requireAuth, getContact);
+router.patch("/contact/:contactId", requireAuth, updateContact);
+router.delete("/contact/:contactId", requireAuth, deleteContact);
 
 export default router;
